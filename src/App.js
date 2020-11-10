ef75baf9a69e97fc1109b2605976c537e147d522
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-11-10 15:42:41 +0530

    router

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import Home from "./components/pages/Home";
 import Contact from './components/pages/Contact';
 import About from './components/pages/About';
+import { BrowserRouter as Router ,Route,Switch} from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import NotFoundPage from './components/pages/NotFoundPage';
 function App() {
   return (
-    <div className="App">
-       <Home/>
-       <About/>
-       <Contact/>
+    <Router>
+   <div className="App">
+       <Navbar/>
+       <Switch>
+      <Route exact path="/" component={Home}></Route>
+      <Route exact path="/contact" component={Contact}></Route>
+      <Route exact path="/about" component={About}></Route>
+      <Route component={NotFoundPage}></Route>
+       </Switch>
+       
     </div>
+  </Router>    
+
   );
 }
 
